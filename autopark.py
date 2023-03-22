c38e554cf524ab20b5ebed79f4ea7420e03ecd36
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-22 23:11:42 +0200

    done task

--- autopark.py
+++ autopark.py
@@ -1,29 +1,29 @@
-# Auto-park
-# DB ->
-#       named as auto =>
-#                       [ scheme of auto :
-#                           {
-#                              "label" : ... ,
-#                              "price" : ... ,
-#                              "wheels" : ... ,
-#                              "color" : ... ,
-#                              "number" : "random value"
-#                           }
-#                        ]
-# Amount of auto > 10
-# color : red , green , black , yellow , blue
-# label : more than 5
-# price : from 2000 till 200 000
-# number and number length > 12
-# Form category dictionary :
-# dynamic
-# cheap (2000 - 20000) , expensive(100000 - 200000) , medium - (20000 - 100000) - class car
-# def sort_car_by_price (arr)
-# return {
-#   cheap : [],
-#   medium : [],
-#   expensive : []
-# }
+# + Auto-park
+# + DB ->
+# +       named as auto =>
+# +                       [ scheme of auto :
+# +                           {
+# +                              "label" : ... ,
+# +                              "price" : ... ,
+# +                              "wheels" : ... ,
+# +                              "color" : ... ,
+# +                              "number" : "random value"
+# +                           }
+# +                        ]
+# + Amount of auto > 10
+# + color : red , green , black , yellow , blue
+# + label : more than 5
+# + price : from 2000 till 200 000
+# + number and number length > 12
+# + Form category dictionary :
+# + dynamic
+# + cheap (2000 - 20000) , expensive(100000 - 200000) , medium - (20000 - 100000) - class car
+# + def sort_car_by_price (arr)
+# + return {
+# +   cheap : [],
+# +   medium : [],
+# +   expensive : []
+# + }
 # print( cars that we have : for poor , rich , and medium-lvl persons )
 # input -> sort : color -> choose on of : blue , green , ... ;  price , label
 # for , while
@@ -31,6 +31,10 @@
 import random
 
 auto_park = []
+cheap_auto = []
+medium_auto = []
+expensive_auto = []
+color_auto = []
 
 label = ['Volkswagen', 'Audi', 'Mercedes-Benz', 'BMW', 'Ford', 'Volvo',
          'Peugeot', 'Renault', 'Daewoo', 'Fiat', 'Seat', 'Chevrolet',
@@ -47,7 +51,7 @@ def random_from(list):  # Function for created random item from list
 
 
 def price_of_auto():  # Function for created random price auto
-    return random.randint(20_000, 200_000)
+    return random.randint(2_000, 200_000)
 
 
 def number_of_engine():  # Function for created random number of engine
@@ -56,11 +60,11 @@ def number_of_engine():  # Function for created random number of engine
 
 # Ask user, how many auto in Auto-park
 numbers_of_auto = int(
-    input("Hello! How much auto in Auto-park would you want? "))
-
+    input("Hello! How much auto in Auto-park would you want?: "))
+print("   Auto-park:")
 # Create Auto-park from user's numbers_of_auto
 for auto in range(numbers_of_auto):
-    auto_default = {
+    random_auto = {
         'label': random_from(label),
         'body type': random_from(body_type),
         'price': price_of_auto(),
@@ -68,19 +72,50 @@ for auto in range(numbers_of_auto):
         'color': random_from(color),
         'number': number_of_engine(),
     }
-    auto_park.append(auto_default)
+    print(f"{auto + 1}) ", random_auto)
+    auto_park.append(random_auto)
 
 
-print("- - - - - - - - - - - - - - - - -")
-print("Auto-park: ", auto_park[0].values())
-print("Auto-park: ", auto_park[1].values())
-print("Auto-park: ", auto_park[2].values())
+# def get_name(auto):
+#     return auto['price']
+# auto_park.sort(key=get_name)
+
+auto_park.sort(key=lambda auto: auto['price'])
 
 
-# def get_name(dictionary):
-#     return dictionary['number']
+for auto in range(numbers_of_auto):
+    if auto_park[auto]['price'] < 20_000:
+        auto_cheap = auto_park[auto]
+        cheap_auto.append(auto_cheap)
+    elif auto_park[auto]['price'] >= 20_000 and auto_park[auto]['price'] < 100_000:
+        auto_medium = auto_park[auto]
+        medium_auto.append(auto_medium)
+    else:
+        auto_expensive = auto_park[auto]
+        expensive_auto.append(auto_expensive)
 
+print("   Cheap auto:")
+for num, auto in enumerate(cheap_auto):
+    print(f"{num + 1}) {auto}")
 
-# sorted_auto_park = auto_park.sort(key=get_name)
+print("   Medium auto:")
+for num, auto in enumerate(medium_auto):
+    print(f"{num + 1}) {auto}")
 
-# print(sorted_auto_park)
+print("   Expensive auto:")
+for num, auto in enumerate(expensive_auto):
+    print(f"{num + 1}) {auto}")
+
+user_color = input("Enter color of auto: ")
+
+for auto in range(numbers_of_auto):
+    if auto_park[auto]['color'] == user_color:
+        auto_color = auto_park[auto]
+        color_auto.append(auto_color)
+
+if color_auto:
+    print(f"   Auto of {user_color} color: ")
+    for num, auto in enumerate(color_auto):
+        print(f"{num + 1}) {auto}")
+else:
+    print(f"   Sorry, there are no auto of {user_color} color.")
